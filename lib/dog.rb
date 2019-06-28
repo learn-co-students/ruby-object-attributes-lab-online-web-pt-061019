7a4b903461fa64ad56a0894c533cd4255242600a
Amber Taranto <ambzzzz@gmail.com>
2019-06-28 01:30:53 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+    def name #getter
+        @name
+    end
+
+    def name=(name) #setter //didn't take "name"
+        @name = name
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+    def name #getter
+        @name
+    end
+
+    def name=(person_name)
+        @name = person_name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+end
