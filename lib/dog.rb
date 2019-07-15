a974e7ea29f7f1d9fb61e55cb8f0a20e22cca7aa
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-15 22:18:20 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+  def initialize
+    @name = name
+  end
+
+def name
+    @name
+
+    def name=(new_name)
+      @name = new_name
+    end
+
+    def name
+      name = "Fido"
+    end
+  def breed=(new_breed)
+     @breed = new_breed
+   end
+
+    def breed
+      breed = "Beagle"
+    end
+end
+end
